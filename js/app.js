3f307b8a94116da0396f6d75057ef39c2e9048d2
Gopika Bejoy <gopikabejoy123@gmail.com>
2018-07-08 15:31:20 +0100

    Added list of icons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const icons = ["fa fa-diamond","fa fa-diamond","fa fa-paper-plane-o","fa fa-paper-plane-o","fa fa-anchor","fa fa-anchor","fa fa-bolt","fa fa-bolt","fa fa-cube","fa fa-cube","fa fa-leaf","fa fa-leaf","fa fa-bicycle","fa fa-bicycle","fa fa-bomb","fa fa-bomb"];
 /*
  * Create a list that holds all of your cards
  */
